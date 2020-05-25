6b945bc2e279bf9ab72867cc5d5f8b4a01879708
stefan judis <stefanjudis@gmail.com>
2020-05-25 14:53:27 +0200

    Add CoC improve app config screen

--- README.md
+++ README.md
@@ -2,18 +2,33 @@
 
 ![Contentful Markdown editor with Alex.js validation](./screenshot.png)
 
-## How to install
+This project is an example of the use of [Contentfuls App Framework](https://www.contentful.com/developers/docs/extensibility/app-framework/), [the design system Forma 36](https://f36.contentful.com/) and [Contentful's open source field editors](https://www.contentful.com/developers/docs/extensibility/field-editors/).
+
+These three tools build the foundation to enrich the Contentful UI.
+
+**The Contentful App Framework** allows you to embed custom source code [in various locations](https://www.contentful.com/developers/docs/extensibility/app-framework/locations/).
+
+**Forma 36** provides lots of React UI components that are ready to be imported in your project to look and feel like native Contentful UI.
+
+**The open source field editors** allow you wrap and change existing components to create the editorial experience you need.
+
+This project includes an example of an App that provides an enriched markdown editor.
+
+## How to install the app
 
 ...
 
-## How to build your own
+## How to develop
 
-...
+To build and use the project you have to have a current [Node.js](https://nodejs.org) version coming with [npm](https://www.npmjs.com) available on your machine.
 
-## Code of Conduct
-
-...
+```
+git clone git@github.com:stefanjudis/contentful-enriched-markdown.git
+cd contentful-enriched-markdown
+npm install
+npm start
+```
 
 ## Licence
 
-...
+[MIT](./LICENSE)
--- index.js
+++ index.js
@@ -1,18 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { init, locations } from 'contentful-ui-extensions-sdk';
-
 import 'codemirror/lib/codemirror.css';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import { MarkdownEditor } from '@contentful/field-editor-markdown';
-import { List, ListItem, Note } from '@contentful/forma-36-react-components';
+import {
+  Button,
+  Heading,
+  List,
+  ListItem,
+  Note,
+  Paragraph,
+} from '@contentful/forma-36-react-components';
 import alex from 'alex';
 
 const ROOT_ELEMENT = document.getElementById('root');
 
 init((sdk) => {
   if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
-    render(<h1>Hello world</h1>, ROOT_ELEMENT);
+    render(
+      <div style={{ maxWidth: '40em', margin: '2em auto' }}>
+        <Heading className="" element="h1" testId="cf-ui-heading">
+          Alex.js enriched Contentful Markdown editor
+        </Heading>
+        <Paragraph>
+          This application is an example to show the use of{' '}
+          <a href="https://www.contentful.com/developers/docs/extensibility/field-editors/">
+            the open source field editors
+          </a>
+          . There is not much to configure here, but you can learn more about
+          the concept by reading the following resources:
+        </Paragraph>
+        <Paragraph style={{ marginTop: '1em' }}>
+          <List>
+            <ListItem>
+              <a href="https://www.contentful.com/developers/docs/extensibility/app-framework/">
+                Contentful App Framework
+              </a>
+            </ListItem>
+            <ListItem>
+              <a href="https://f36.contentful.com/">Forma 36 design system</a>
+            </ListItem>
+            <ListItem>
+              <a href="https://www.contentful.com/developers/docs/extensibility/field-editors/">
+                Open source field editors
+              </a>
+            </ListItem>
+            <ListItem>Soon... Tutorial for this app</ListItem>
+          </List>
+        </Paragraph>
+        <Button
+          href="https://github.com/stefanjudis/contentful-enriched-markdown"
+          style={{ marginTop: '1em' }}
+          target="_blank"
+        >
+          Read more on GitHub
+        </Button>
+      </div>,
+
+      ROOT_ELEMENT
+    );
     sdk.app.setReady();
   } else if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
     render(<MyMarkdownEditor sdk={sdk} />, ROOT_ELEMENT);
