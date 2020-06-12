e63443d9d10c8aa747390324730b1b5eb045d9ed
stefan judis <stefanjudis@gmail.com>
2020-06-12 15:18:29 +0200

    Make use of optional config screen

--- index.js
+++ index.js
@@ -3,67 +3,21 @@ import { render } from 'react-dom';
 import { init, locations } from 'contentful-ui-extensions-sdk';
 import 'codemirror/lib/codemirror.css';
 import '@contentful/forma-36-react-components/dist/styles.css';
-import { MarkdownEditor } from '@contentful/field-editor-markdown';
 import {
-  Button,
-  Heading,
-  List,
-  ListItem,
-  Note,
-  Paragraph,
-} from '@contentful/forma-36-react-components';
+  MarkdownEditor,
+  renderMarkdownDialog,
+} from '@contentful/field-editor-markdown';
+import { List, ListItem, Note } from '@contentful/forma-36-react-components';
 import alex from 'alex';
 
 const ROOT_ELEMENT = document.getElementById('root');
 
 init((sdk) => {
-  if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
-    render(
-      <div style={{ maxWidth: '40em', margin: '2em auto' }}>
-        <Heading className="" element="h1" testId="cf-ui-heading">
-          Alex.js enriched Contentful Markdown editor
-        </Heading>
-        <Paragraph>
-          This application is an example to show the use of{' '}
-          <a href="https://www.contentful.com/developers/docs/extensibility/field-editors/">
-            the open source field editors
-          </a>
-          . There is not much to configure here, but you can learn more about
-          the concept by reading the following resources:
-        </Paragraph>
-        <Paragraph style={{ marginTop: '1em' }}>
-          <List>
-            <ListItem>
-              <a href="https://www.contentful.com/developers/docs/extensibility/app-framework/">
-                Contentful App Framework
-              </a>
-            </ListItem>
-            <ListItem>
-              <a href="https://f36.contentful.com/">Forma 36 design system</a>
-            </ListItem>
-            <ListItem>
-              <a href="https://www.contentful.com/developers/docs/extensibility/field-editors/">
-                Open source field editors
-              </a>
-            </ListItem>
-            <ListItem>Soon... Tutorial for this app</ListItem>
-          </List>
-        </Paragraph>
-        <Button
-          href="https://github.com/stefanjudis/contentful-enriched-markdown"
-          style={{ marginTop: '1em' }}
-          target="_blank"
-        >
-          Read more on GitHub
-        </Button>
-      </div>,
-
-      ROOT_ELEMENT
-    );
-    sdk.app.setReady();
-  } else if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
+  if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
     render(<MyMarkdownEditor sdk={sdk} />, ROOT_ELEMENT);
     sdk.window.startAutoResizer();
+  } else if (sdk.location.is(locations.LOCATION_DIALOG)) {
+    render(renderMarkdownDialog(sdk), ROOT_ELEMENT);
   }
 });
 
@@ -71,9 +25,13 @@ function MyMarkdownEditor({ sdk }) {
   const [warnings, setWarnings] = useState([]);
 
   useEffect(() => {
-    sdk.field.onValueChanged((value) => {
+    const unsubscribe = sdk.field.onValueChanged((value) => {
       setWarnings(alex.markdown(value).messages);
     });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
